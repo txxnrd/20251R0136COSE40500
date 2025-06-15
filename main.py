8757af0963c968d53104cba407681e8486f98425
txxnrd <nrbsld@korea.ac.kr>
2025-06-15 14:22:16 +0900

    first commit for 20251R0136COSE405003

---
+++ main.py
@@ -0,0 +1,5 @@
+###### 20251R0136COSE405003 #######
+# Title: Simple Sentiment Analyzer
+# Description: This Python script implements a basic sentiment analyzer.
+# It uses predefined positive and negative word lists to classify user input
+# as positive, negative, or neutral sentiment. No machine learning is used.
