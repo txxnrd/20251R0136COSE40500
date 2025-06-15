70e2f0944e7a93b3f5a712ad432d153100e76932
txxnrd <nrbsld@korea.ac.kr>
2025-06-15 14:23:49 +0900

    vocabs added

--- main.py
+++ main.py
@@ -3,3 +3,6 @@
 # Description: This Python script implements a basic sentiment analyzer.
 # It uses predefined positive and negative word lists to classify user input
 # as positive, negative, or neutral sentiment. No machine learning is used.
+
+positive_words = ["good", "great", "happy", "awesome", "fantastic", "love","brilliant","fabulous"]
+negative_words = ["bad", "terrible", "sad", "horrible", "hate", "worst","awful"]
